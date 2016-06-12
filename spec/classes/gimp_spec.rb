e90f4969e5604face101d67c262c9a9dc59c131b
Romuald du Song <romuald.dusong@deveryware.com>
2016-06-12 18:06:38 +0200

    remove obsolete ruby 1.8.7

--- spec/classes/gimp_spec.rb
+++ spec/classes/gimp_spec.rb
@@ -6,11 +6,8 @@ describe 'gimp' do
   version = '2.8.16-x86_64-1'
 
   it { should contain_class('gimp') }
+  it { should contain_package("Gimp-#{version}").with_provider('appdmg') }
+  it { should contain_package("Gimp-#{version}")
+    .with_source("http://download.gimp.org/mirror/pub/gimp/v#{major_version}/osx/gimp-#{version}.dmg") }
 
-  it do
-    should contain_package("Gimp-#{version}").with({
-      :provider => 'appdmg',
-      :source   => "http://download.gimp.org/mirror/pub/gimp/v#{major_version}/osx/gimp-#{version}.dmg",
-    })
-  end
 end
