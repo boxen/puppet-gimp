17ce6fb5e00dc2091d0e87b1e5e6b21370945f18
Romuald du Song <romuald.dusong@deveryware.com>
2016-06-12 17:42:03 +0200

    fix syntax in test

--- spec/classes/gimp_spec.rb
+++ spec/classes/gimp_spec.rb
@@ -5,8 +5,10 @@ describe 'gimp' do
   major_version = '2.8'
   version = '2.8.16-x86_64-1'
 
+  it { should contain_class('gimp') }
+
   it do
-    should contain_package("Gimp-${version}").with({
+    should contain_package("Gimp-#{version}").with({
       :provider => 'appdmg',
       :source   => "http://download.gimp.org/mirror/pub/gimp/v#{major_version}/osx/gimp-#{version}.dmg",
     })
