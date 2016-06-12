1f62b5f77abfd8bbce4e79b4b00537c3a05adc5a
Romuald du Song <romuald.dusong@deveryware.com>
2016-06-12 18:06:12 +0200

    correct url source

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,6 @@
 class gimp($major_version='2.8', $version='2.8.16-x86_64-1') {
   package { "Gimp-${version}":
     provider => 'appdmg',
-    source   => "http://download.gimp.org/pub/gimp/v${major_version}/osx/gimp-${version}.dmg",
+    source   => "http://download.gimp.org/mirror/pub/gimp/v${major_version}/osx/gimp-${version}.dmg",
   }
 }
